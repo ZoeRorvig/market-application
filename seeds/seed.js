6a30d01ee1e59637fa0040ac56dc52be8e913eb5
Terrance Smith <terrancesmith@laquishas-air.mynetworksettings.com>
2023-01-19 13:54:48 -0500

    added category seed

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,6 +1,7 @@
 const sequelize = require('../config/connection');
 const seedProducts = require('./productData');
 const seedUser = require('./userData');
+const seedCategories = require('./categoryData');
 
 const init = async () => {
   await sequelize.sync({ force: true });
@@ -8,6 +9,9 @@ const init = async () => {
   await seedProducts();
 
   await seedUser();
+
+  await seedCategories();
+  
   process.exit(0);
 };
 

32cf970a1f5000bff6e83176aacf55f872e01063
Chris <106836962+Cujion@users.noreply.github.com>
2023-01-19 12:39:03 -0500

    started Category model, updated product model, updated index.js in model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Product = require('./Product');
+const Category = require('./Category');
 
 User.hasMany(Product, {
   foreignKey: 'user_id'
@@ -9,4 +10,8 @@ Product.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Product };
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
+module.exports = { User, Product, Category };

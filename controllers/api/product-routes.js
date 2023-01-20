b2d8261333683d7c4506191fa1520641311724af
Deiontre10 <travontazlowry@gmail.com>
2023-01-20 13:55:18 -0500

    fixed path for withAuth

--- controllers/api/product-routes.js
+++ controllers/api/product-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Product, User, Category } = require('../../models');
-const withAuth = require('../utils/auth');
+const withAuth = require('../../utils/auth');
 
 
 // GET all products

c536a304c2fc7539e1423cb6e7179afaf1e28d84
Chris <106836962+Cujion@users.noreply.github.com>
2023-01-23 10:06:35 -0500

    updated styling and added post a product layout

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,4 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
+    var instances = M.Sidenav.init(elems); 
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems);
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems);
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems);
   });

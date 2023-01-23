75670f5674b101ed8ff7f3289ef09dccb4a1372d
Deiontre10 <travontazlowry@gmail.com>
2023-01-23 14:13:30 -0500

    got the post to work for dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,22 +1,21 @@
 const createPostHandler = async function (event) {
     event.preventDefault();
 
-    const productTitleEl = document.querySelector('title');
-    const productDescriptionEl = document.querySelector('description');
-    const productPriceEl = document.querySelector('price');
-    const productImageEl = document.querySelector('image');
-    const userEmailEl = document.querySelector('email');
-    const productCategory = document.querySelector('dropdown1');
-    const postProductButton = document.querySelector('post');
+    const productTitleEl = document.querySelector('#title');
+    const productDescriptionEl = document.querySelector('#description');
+    const productPriceEl = document.querySelector('#price');
+    const productImageEl = document.querySelector('#image');
+    const userEmailEl = document.querySelector('#email');
+    const productCategory = document.querySelector('#select-menu');
 
-    const response = await fetch('/dashboard', {
+    const response = await fetch('/api/products', {
         method: 'POST',
         body: JSON.stringify({
             title: productTitleEl.value,
             description: productDescriptionEl.value,
             price: productPriceEl.value,
             image: productImageEl.value,
-            email: userEmailEl.value,
+            // email: userEmailEl.value,
             category_id: productCategory.value
         }),
         headers: { 'Content-Type': 'application/json' },
@@ -29,23 +28,5 @@ const createPostHandler = async function (event) {
     }
 };
 
-document
+document.querySelector('#slide-out')
     .addEventListener('submit', createPostHandler);
-
-document
-    .addEventListener('click', postProductButton);
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems);
-    });
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.tooltipped');
-    var instances = M.Tooltip.init(elems);
-    });
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems);
-    });
--- public/js/homepage.js
+++ public/js/homepage.js
@@ -17,3 +17,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+    });
